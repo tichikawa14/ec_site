20f86dcd958b76f6d3f3bf1a02908c47f5b5aa7c
EC2 Default User <ec2-user@ip-172-31-31-25.ap-southeast-1.compute.internal>
2019-03-07 02:14:35 +0000

    add faker and product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190306060346) do
+ActiveRecord::Schema.define(version: 20190306143313) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do |n|
+  name  = Faker::Commerce.product_name
+  description = Faker::Lorem.sentence
+  price = Faker::Number.within(1..100000)
+  Product.create!(name:  name,
+               description:  description,
+               price: price)
+end
