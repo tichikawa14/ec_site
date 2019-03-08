eb72ae024e65260d4cbc7b503ed7da2e855331ee
EC2 Default User <ec2-user@ip-172-31-31-25.ap-southeast-1.compute.internal>
2019-03-08 05:15:00 +0000

    Use SSL and the Puma webserver in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  
+  config.force_ssl = true
   # mailer setteings
   config.action_mailer.default_url_options = { host: 'a8cd08b168944f30ac5ffcbc2c605b52.vfs.cloud9.ap-southeast-1.amazonaws.com', protocol: 'https' }
 
