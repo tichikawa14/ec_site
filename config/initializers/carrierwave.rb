47fddcb3abd092276f5f3f5ae56ee46b84cf06b2
EC2 Default User <ec2-user@ip-172-31-31-25.ap-southeast-1.compute.internal>
2019-03-11 09:25:07 +0000

    change carrierwabe.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,11 +6,9 @@ CarrierWave.configure do |config|
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],                        # required unless using use_iam_profile
       aws_secret_access_key: ENV['S3_SECRET_KEY'],                        # required unless using use_iam_profile
       region:                ENV['S3_REGION'],                  # optional, defaults to 'us-east-1'
-      host:                  's3.example.com',             # optional, defaults to nil
-      endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
     }
     config.fog_directory  = ENV['S3_BUCKET']                                      # required
-    config.fog_public     = false                                                 # optional, defaults to true
+
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
   else
     config.storage :file
