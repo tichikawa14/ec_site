5a8198a49b4d8d836132bf271b59324822c5f9ba
EC2 Default User <ec2-user@ip-172-31-31-25.ap-southeast-1.compute.internal>
2019-03-11 09:19:12 +0000

    delete use_iam_profile

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,6 @@ CarrierWave.configure do |config|
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],                        # required unless using use_iam_profile
       aws_secret_access_key: ENV['S3_SECRET_KEY'],                        # required unless using use_iam_profile
-      use_iam_profile:       true,                         # optional, defaults to false
       region:                ENV['S3_REGION'],                  # optional, defaults to 'us-east-1'
       host:                  's3.example.com',             # optional, defaults to nil
       endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
